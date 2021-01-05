b34371ebd732a517e32491db2ff666ac7555157c
ZengGui <anoob@sina.cn>
2021-01-05 17:57:03 +0800

    refactor: 💡 find eigen

--- Findeigen.cmake
+++ Findeigen.cmake
@@ -7,7 +7,9 @@
 
 find_path(eigen_INCLUDE_DIR Eigen/Cholesky
     HINTS "$ENV{CX_THIRDPARTY_ROOT}/include/eigen/"
-	PATHS "/usr/include/eigen")
+	PATHS "/usr/local/include/eigen"
+	NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH
+	)
 	
 if(eigen_INCLUDE_DIR)
 	set(eigen_INCLUDE_DIRS ${eigen_INCLUDE_DIR})
