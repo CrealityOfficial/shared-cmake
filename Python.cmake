242626ba0553a6e03460ac760f0c9708d10e8398
anoob <anoob@sian.cn>
2021-03-30 13:42:52 +0800

    Python.cmake

---
+++ Python.cmake
@@ -0,0 +1,16 @@
+find_package(Python3 REQUIRED COMPONENTS Interpreter Development)
+    
+if(Python3_FOUND AND Python3_Development_FOUND)
+	message(STATUS "Find Python3 ${Python3_VERSION}")
+	message(STATUS "INCLUDES : ${Python3_INCLUDE_DIRS}")
+	message(STATUS "LIBRARIES : ${Python3_LIBRARIES}")
+	message(STATUS "LIBRARY DIRS : ${Python3_LIBRARY_DIRS}")
+else()
+	message(FATAL_ERROR "Can't find Python3.")
+endif()
+
+list(GET Python3_LIBRARIES 3 Python3_LIBRARIES_DEBUG)
+list(GET Python3_LIBRARIES 1 Python3_LIBRARIES_RELEASE)
+message(STATUS "Python3 Debug LIBRARIES : ${Python3_LIBRARIES_DEBUG}")
+message(STATUS "Python3 Release LIBRARIES : ${Python3_LIBRARIES_RELEASE}")
+__import_target(Python3 dll)
