0190ddda37fcfa6952cd84ca8a478a513e14d6ec
Sunday251314 <heyunteng1314@sina.com>
2021-04-01 18:46:05 +0800

    change boost lib file for realse/debug

--- Boost.cmake
+++ Boost.cmake
@@ -2,8 +2,10 @@ macro(__add_boost_target module)
 	__source_recurse(${CMAKE_CURRENT_SOURCE_DIR} SRC)
 	__source_recurse(${boost_includes}boost/${module}/ HEADER)
 	
-	__add_real_target(boost_${module} dll SOURCE ${SRC} ${HEADER})
-	set_property(TARGET boost_${module} PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${boost_includes})
+	__add_real_target(boost_${module} dll SOURCE ${SRC} ${HEADER}
+	  INC ${CMAKE_CURRENT_SOURCE_DIR}/../../
+	  DEF BOOST_ALL_DYN_LINK
+	  INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/../../)
 endmacro()
 
 macro(__find_boost_root)
