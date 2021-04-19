a30fa1758959c0f4fc2455120b266a07fe993cce
Sunday251314 <heyunteng1314@sina.com>
2021-04-19 19:33:43 +0800

    change boot cmake

--- Findqhull.cmake
+++ Findqhull.cmake
@@ -22,14 +22,14 @@ find_library(qhullcpp_LIBRARIES_RELEASE
 			 NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH
 			 )
 		
-find_library(qhullstatic_LIBRARIES_DEBUG
+find_library(qhullstatic_r_LIBRARIES_DEBUG
              NAMES qhullstatic_r
              HINTS "$ENV{CX_THIRDPARTY_ROOT}/lib/debug"
 			 PATHS "/usr/local/lib/Debug"
 			 NO_SYSTEM_ENVIRONMENT_PATH NO_CMAKE_SYSTEM_PATH NO_CMAKE_PATH NO_CMAKE_ENVIRONMENT_PATH
 			 )
 			 
-find_library(qhullstatic_LIBRARIES_RELEASE
+find_library(qhullstatic_r_LIBRARIES_RELEASE
              NAMES qhullstatic_r
              HINTS "$ENV{CX_THIRDPARTY_ROOT}/lib/release"
 			 PATHS "/usr/local/lib/Release"
@@ -39,16 +39,16 @@ find_library(qhullstatic_LIBRARIES_RELEASE
 message(STATUS ${qhull_INCLUDE_DIRS})
 message(STATUS ${qhullcpp_LIBRARIES_DEBUG})
 message(STATUS ${qhullcpp_LIBRARIES_RELEASE})
-message(STATUS ${qhullstatic_LIBRARIES_DEBUG})
-message(STATUS ${qhullstatic_LIBRARIES_RELEASE})
+message(STATUS ${qhullstatic_r_LIBRARIES_DEBUG})
+message(STATUS ${qhullstatic_r_LIBRARIES_RELEASE})
 
 if(qhull_INCLUDE_DIRS AND qhullcpp_LIBRARIES_DEBUG AND qhullcpp_LIBRARIES_RELEASE
-					AND qhullstatic_LIBRARIES_DEBUG AND qhullstatic_LIBRARIES_RELEASE)
+					AND qhullstatic_r_LIBRARIES_DEBUG AND qhullstatic_r_LIBRARIES_RELEASE)
 	set(qhull_FOUND "True")
 	set(qhullcpp_INCLUDE_DIRS ${qhull_INCLUDE_DIRS})
-	set(qhullstatic_INCLUDE_DIRS ${qhull_INCLUDE_DIRS})
+	set(qhullstatic_r_INCLUDE_DIRS ${qhull_INCLUDE_DIRS})
 
 	__import_target(qhullcpp lib)
-	__import_target(qhullstatic lib)
-	set(qhull qhullcpp qhullstatic)
+	__import_target(qhullstatic_r lib)
+	set(qhull qhullcpp qhullstatic_r)
 endif()
