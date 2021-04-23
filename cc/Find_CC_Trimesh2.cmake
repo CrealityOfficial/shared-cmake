1d25bfe01bd46fae2c339b7845c464eda018b145
anoob <anoob@sian.cn>
2021-04-23 15:52:31 +0800

    find trimesh2

--- cc/Find_CC_Trimesh2.cmake
+++ cc/Find_CC_Trimesh2.cmake
@@ -1,12 +1,12 @@
 # This sets the following variables:
 # trimesh2 target
 
-if(THIRD0_INSTALL_ROOT)
-	message(STATUS "Specified THIRD0_INSTALL_ROOT : ${THIRD0_INSTALL_ROOT}")
-	set(trimesh2_INCLUDE_ROOT ${THIRD0_INSTALL_ROOT}/include/zlib/)
-	set(trimesh2_LIB_ROOT ${THIRD0_INSTALL_ROOT}/lib/)
+if(THIRD1_INSTALL_ROOT)
+	message(STATUS "Specified THIRD1_INSTALL_ROOT : ${THIRD1_INSTALL_ROOT}")
+	set(trimesh2_INCLUDE_ROOT ${THIRD1_INSTALL_ROOT}/include/)
+	set(trimesh2_LIB_ROOT ${THIRD1_INSTALL_ROOT}/lib/)
 	__search_target_components(trimesh2
-							   INC zlib.h
+							   INC trimesh2/TriMesh.h
 							   DLIB trimesh2
 							   LIB trimesh2
 							   )
