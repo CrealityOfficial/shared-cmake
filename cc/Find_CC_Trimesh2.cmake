49be7cb8cd09d1cb18010f8f14c9ea3f37cb1d8f
anoob <anoob@sina.cn>
2021-07-29 20:03:24 +0800

    find trimesh2
    Change-Id: I8f69f8e94553ee8b35611e7cf7ea1ed8ac0dcc1b

--- cc/Find_CC_Trimesh2.cmake
+++ cc/Find_CC_Trimesh2.cmake
@@ -5,20 +5,16 @@ if(THIRD1_INSTALL_ROOT)
 	message(STATUS "Specified THIRD1_INSTALL_ROOT : ${THIRD1_INSTALL_ROOT}")
 	set(trimesh2_INCLUDE_ROOT ${THIRD1_INSTALL_ROOT}/include/)
 	set(trimesh2_LIB_ROOT ${THIRD1_INSTALL_ROOT}/lib/)
-	__search_target_components(trimesh2
-							   INC trimesh2/TriMesh.h
-							   DLIB trimesh2
-							   LIB trimesh2
-							   )
 elseif(CXTRIMESH2_INSTALL_ROOT)
 	message(STATUS "Specified CXTRIMESH2_INSTALL_ROOT : ${CXTRIMESH2_INSTALL_ROOT}")
 	set(trimesh2_INCLUDE_ROOT ${CXTRIMESH2_INSTALL_ROOT}/include/)
-	set(trimesh2_LIB_ROOT ${CXTRIMESH2_INSTALL_ROOT}/lib/)
-	__search_target_components(trimesh2
-							   INC trimesh2/TriMesh.h
-							   DLIB trimesh2
-							   LIB trimesh2
-							   )
+	set(trimesh2_LIB_ROOT ${CXTRIMESH2_INSTALL_ROOT}/lib/)	
 endif()
+
+__search_target_components(trimesh2
+						   INC trimesh2/TriMesh.h
+						   DLIB trimesh2
+						   LIB trimesh2
+						   )
 
 __test_import(trimesh2 lib)
