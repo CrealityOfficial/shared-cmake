80f186b0f70d3dd9461e1c436401c38477b7c18e
anoob <anoob@sina.cn>
2021-07-30 13:05:28 +0800

    ci

--- ci/build-win32.bat
+++ ci/build-win32.bat
@@ -1,12 +1,12 @@
 @echo off
 
-call "%~dp0\..\package\find-msvcinfo.bat"
+call "%~dp0\find-msvcinfo.bat"
 if not %errorlevel%==0 (exit /b 1)
 		
 call "%VSDir%\VC\Auxiliary\Build\vcvars64.bat"
 
 REM 从注册表查询 win10 SDK 安装目录，主要用于判断注册表查询是否被禁，如果被禁 vcvars64.bat 将无法正常配置环境，需要手动配置环境依赖
-call "%~dp0\..\package\check-vcvars64.bat"
+call "%~dp0\check-vcvars64.bat"
 
 :build
 echo "build"
