6534c3d13219e7e755c46a6fcf9bf679eacdc013
anoob <anoob@sina.cn>
2021-09-29 12:19:06 +0800

    tracer

--- ccglobal/tracer.h
+++ ccglobal/tracer.h
@@ -1,6 +1,9 @@
 #ifndef TRACER_1630734954343_H
 #define TRACER_1630734954343_H
 
+#include <stdarg.h>
+#include <stdio.h>
+
 namespace ccglobal
 {
 	/*
@@ -20,6 +23,16 @@ namespace ccglobal
 		virtual void message(const char* msg) = 0;
 		virtual void failed(const char* msg) = 0;
 		virtual void success() = 0;
+
+		void formatMessage(const char* format, ...)
+		{
+			char buf[1024] = { 0 };
+			va_list args;
+			va_start(args, format);
+			vsprintf(buf, format, args);
+			message(buf);
+			va_end(args);
+		}
 	};
 }
 #endif // TRACER_1630734954343_H
