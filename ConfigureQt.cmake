fcaa6e9882606bac5cbd01071a8d941f5dccc9b1
zhengfeiyan <zhengfeiyan@creality.com>
2021-12-13 14:08:26 +0800

    加入 WebSockets模块

--- ConfigureQt.cmake
+++ ConfigureQt.cmake
@@ -4,7 +4,7 @@ macro(__enable_qt5)
 	set(CMAKE_AUTOMOC ON)
 	set(CMAKE_AUTORCC ON)
 
-	find_package(Qt5 COMPONENTS Core Widgets Gui Quick Qml Xml 3DCore 3DRender 3DExtras 3DInput 3DLogic 3DQuick SerialPort Concurrent OpenGL REQUIRED)
+	find_package(Qt5 COMPONENTS Core Widgets Gui Quick Qml Xml 3DCore 3DRender 3DExtras 3DInput 3DLogic 3DQuick SerialPort Concurrent OpenGL REQUIRED WebSockets)
 	include(qml)
 	
 	set(QT5_ENABLED 1)
