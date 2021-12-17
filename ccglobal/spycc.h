8a9c067192dcd7a126a7334efeb44d7da4fde084
anoob <anoob@sina.cn>
2021-12-17 20:05:55 +0800

    spycc

--- ccglobal/spycc.h
+++ ccglobal/spycc.h
@@ -10,12 +10,12 @@
 #define ANALYSIS_TICK(x) SESSION_TICK(x)
 #define ANALYSIS_DUMP(x) SESSION_DUMP(x)
 #else
-#define ANALYSIS_START(x) void(x);
-#define ANALYSIS_TICK(x) void(x);
-#define ANALYSIS_DUMP(x) void(x);
-#define SESSION_START(x) void(x);
-#define SESSION_TICK(x) void(x);
-#define SESSION_DUMP(x) void(x);
+#define ANALYSIS_START(x) void(x)
+#define ANALYSIS_TICK(x) void(x)
+#define ANALYSIS_DUMP(x) void(x)
+#define SESSION_START(x) void(x)
+#define SESSION_TICK(x) void(x)
+#define SESSION_DUMP(x) void(x)
 #endif
 
 #endif
