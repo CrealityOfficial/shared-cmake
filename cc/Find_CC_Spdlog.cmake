6d3828646865fb437fc285eb5975835f0a4866d3
liuhong <liuhong@creality.com>
2022-01-12 19:52:06 +0800

    spdlogg

--- cc/Find_CC_Spdlog.cmake
+++ cc/Find_CC_Spdlog.cmake
@@ -1,19 +1,10 @@
 # This sets the following variables:
 # spdlog target
-
-if(THIRD1_INSTALL_ROOT)
-	message(STATUS "Specified THIRD1_INSTALL_ROOT : ${THIRD1_INSTALL_ROOT}")
-	set(spdlog_INCLUDE_ROOT ${THIRD1_INSTALL_ROOT}/include/spdlog/)
-	set(spdlog_LIB_ROOT ${THIRD1_INSTALL_ROOT}/lib/)							 
-elseif(CXSPDLOG_INSTALL_ROOT)
-	set(spdlog_INCLUDE_ROOT ${CXSPDLOG_INSTALL_ROOT}/include/spdlog/)
-	set(spdlog_LIB_ROOT ${CXSPDLOG_INSTALL_ROOT}/lib/)
-endif()
-
 __search_target_components(spdlog
-						   INC spdlog/spdlog.h
+						   INC spdlog/cxlog.h
 						   DLIB spdlog
 						   LIB spdlog
-						   )	
+						   PRE spdlog
+						   )
 
 __test_import(spdlog dll)
