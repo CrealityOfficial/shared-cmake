80f2e49aa367e6e8bef4789e4987676fc57f2dbc
anoob <anoob@sina.cn>
2022-02-16 14:08:42 +0800

    linux ci

--- ci/build-linux.sh
+++ ci/build-linux.sh
@@ -13,8 +13,8 @@ cd linux-build
 mkdir build
 cd build
 
-cmake ../../ -DCMAKE_BUILD_TYPE=Debug
-make -j8
+# cmake ../../ -DCMAKE_BUILD_TYPE=Debug
+# make -j8
 cmake ../../ -DCMAKE_BUILD_TYPE=Release
 make -j8
 sudo make install
