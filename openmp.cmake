e8c30b5d4aae6121be8c1759bd53e55c503fceb8
baijunjiang <baijunjiang@creality.com>
2022-03-07 10:50:40 +0800

    standardize openMP

--- openmp.cmake
+++ openmp.cmake
@@ -1,21 +1,25 @@
 macro(__enable_openmp)
 	find_package(OpenMP)
 	if(OPENMP_FOUND)
+		message("__enable_opemmp Find OpenMP.")
+		if(TARGET OpenMP::OpenMP_CXX)
+			message("OpenMP TARGET OpenMP::OpenMP_CXX Imported.")
+		endif()
 		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
 		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
 		set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${OpenMP_EXE_LINKER_FLAGS}")
 	endif()
 endmacro()
 
-macro(__add_openmp_lib arg1)
-    find_library(OPENOMP_LIBRARIES
-             NAMES omp
-             PATHS "/usr/local/lib")
-    message(STATUS ${OPENOMP_LIBRARIES})
-	if(OPENOMP_LIBRARIES)
-		target_link_libraries(${arg1} PRIVATE ${OPENOMP_LIBRARIES})
-	endif()
-endmacro()
+#macro(__add_openmp_lib arg1)
+#    find_library(OPENOMP_LIBRARIES
+#             NAMES omp
+#             PATHS "/usr/local/lib")
+#    message(STATUS ${OPENOMP_LIBRARIES})
+#	if(OPENOMP_LIBRARIES)
+#		target_link_libraries(${arg1} PRIVATE ${OPENOMP_LIBRARIES})
+#	endif()
+#endmacro()
 
 macro(__disable_openmp)
 	string(REPLACE "-openmp" "" CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
