1cc0308646fe3907d557e0405b6cd382a8fa1287
anoob <anoob@sina.cn>
2022-03-13 18:16:05 +0800

    serial drill

--- ccglobal/serial/serialdrill.h
+++ ccglobal/serial/serialdrill.h
@@ -2,20 +2,32 @@
 #define CCGLOBAL_SERIAL_DRILL_H
 #include "ccglobal/serial/serialtrimesh.h"
 
-void cc_load_drill(const std::string& file, trimesh::fxform& xf, trimesh::TriMesh& mesh)
+struct CCDrillParam
 {
-	auto f = [&xf, &mesh](std::fstream& in) {
+	int count;
+	float radius;
+	float depth;
+	trimesh::vec3 position;
+	trimesh::vec3 normal;
+	bool onlyOneLayer;
+};
+
+void cc_load_drill(const std::string& file, trimesh::fxform& xf, trimesh::TriMesh& mesh, std::vector<CCDrillParam>& params)
+{
+	auto f = [&xf, &mesh, &params](std::fstream& in) {
 		loadFXform(in, xf);
 		loadTrimesh(in, mesh);
+		loadVectorT(in, params);
 	};
 	serialLoad(file, f);
 }
 
-void cc_save_drill(const std::string& file, trimesh::fxform& xf, trimesh::TriMesh& mesh)
+void cc_save_drill(const std::string& file, trimesh::fxform& xf, trimesh::TriMesh& mesh, std::vector<CCDrillParam>& params)
 {
-	auto f = [&xf, &mesh](std::fstream& out) {
+	auto f = [&xf, &mesh, &params](std::fstream& out) {
 		saveFXform(out, xf);
 		saveTrimesh(out, mesh);
+		saveVectorT(out, params);
 	};
 	serialSave(file, f);
 }
