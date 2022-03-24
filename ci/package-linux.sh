f216dfa51a20b65237dba7d18ebf3fdb9f1b278c
anoob <anoob@sina.cn>
2022-03-24 19:22:04 +0800

    package

--- ci/package-linux.sh
+++ ci/package-linux.sh
@@ -3,7 +3,6 @@ if [ -z $1 ]; then
 	echo "build Usage:"
 	echo "Only build: build.sh v0.0.1"
 	echo "Build and package:build.bat v0.1.0 package"
-	exit 0
 fi
 if [ -z $3 ]; then
 	VERSION_EXTRA="test"
@@ -25,8 +24,6 @@ cd $workdir
 cd ..
 mkdir out
 cd out
-cmake ../ -DCMAKE_BUILD_TYPE=Debug -DPROJECT_VERSION_MAJOR=${MAJOR} -DPROJECT_VERSION_MINOR=${MINOR}  -DPROJECT_VERSION_PATCH=${PATCH} -DPROJECT_BUILD_ID=${BUILD} -DPROJECT_VERSION_EXTRA=${VERSION_EXTRA}
-make -j8
 cmake ../ -DCMAKE_BUILD_TYPE=Release -DPROJECT_VERSION_MAJOR=${MAJOR} -DPROJECT_VERSION_MINOR=${MINOR}  -DPROJECT_VERSION_PATCH=${PATCH} -DPROJECT_BUILD_ID=${BUILD} -DPROJECT_VERSION_EXTRA=${VERSION_EXTRA}
 make -j8
 sudo make install
