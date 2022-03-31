4deec1f7a2a86a9bbfb6181bfb4ccf8bb143f831
baijunjiang <baijunjiang@creality.com>
2022-03-31 17:03:20 +0800

    change mac resources build
    Change-Id: Ia21023f0799841246a04e19fb2b379dc6efc5a4f

--- ConfigureTarget.cmake
+++ ConfigureTarget.cmake
@@ -231,9 +231,9 @@ function(__add_real_target target type)
 								)
 					elseif(CC_BC_MAC)
 						add_custom_command(TARGET ${target} POST_BUILD
-								COMMAND ${CMAKE_COMMAND} -E make_directory "$<TARGET_FILE_DIR:${target}>/../Frameworks/${plugin}/"
-								COMMAND ${CMAKE_COMMAND} -E copy ${DIR_NAME} "$<TARGET_FILE_DIR:${target}>/../Frameworks/${plugin}/"
-								COMMAND ${CMAKE_COMMAND} -E copy "$<TARGET_FILE_DIR:${plugin}>/${targetName}" "$<TARGET_FILE_DIR:${target}>/../Frameworks/${plugin}/"
+								COMMAND ${CMAKE_COMMAND} -E make_directory "$<TARGET_FILE_DIR:${target}>/../Resources/qml/${plugin}/"
+								COMMAND ${CMAKE_COMMAND} -E copy ${DIR_NAME} "$<TARGET_FILE_DIR:${target}>/../Resources/qml/${plugin}/"
+								COMMAND ${CMAKE_COMMAND} -E copy "$<TARGET_FILE_DIR:${plugin}>/${targetName}" "$<TARGET_FILE_DIR:${target}>/../Resources/qml/${plugin}/"
 								)
 					endif()
 				else()
