1713a8f5dae425fd73846f80a321ba04f8a9ba02
anoob <anoob@sina.cn>
2022-04-01 10:17:04 +0800

    Find CC Nlopt

--- cc/Find_CC_Clipper3r.cmake
+++ cc/Find_CC_Clipper3r.cmake
@@ -5,11 +5,13 @@ if(THIRD1_INSTALL_ROOT)
 elseif(CXCLIPPER_INSTALL_ROOT)
 endif()
 
-__search_target_components(clipper3r
-						   INC clipper3r/clipper.hpp
-						   DLIB clipper3r
-						   LIB clipper3r
-						   PRE clipper3r
-						   )
-
-__test_import(clipper3r lib)
+if(NOT TARGET clipper3r)
+	__search_target_components(clipper3r
+							INC clipper3r/clipper.hpp
+							DLIB clipper3r
+							LIB clipper3r
+							PRE clipper3r
+							)
+	
+	__test_import(clipper3r lib)
+endif()
--- cc/Find_CC_Nlopt.cmake
+++ cc/Find_CC_Nlopt.cmake
@@ -1,11 +1,13 @@
 # This sets the following variables:
 # nlopt target
 
-__search_target_components(nlopt
-						   INC nlopt.hpp
-						   DLIB nlopt
-						   LIB nlopt
-						   PRE nlopt
-						   )
-
-__test_import(nlopt dll)
+if(NOT TARGET nlopt)
+	__search_target_components(nlopt
+							INC nlopt.hpp
+							DLIB nlopt
+							LIB nlopt
+							PRE nlopt
+							)
+	
+	__test_import(nlopt dll)
+endif()
