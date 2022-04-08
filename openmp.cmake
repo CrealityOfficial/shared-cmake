709c8a20993641de0127d5d0475dbdf5dd064e95
liuhong <liuhong@creality.com>
2022-04-08 12:08:33 +0800

    opemp alians

--- openmp.cmake
+++ openmp.cmake
@@ -1,50 +1,56 @@
 #target OpenMP::OpenMP_CXX
 
 macro(__enable_openmp)
-if(CC_BC_WIN OR CC_BC_MAC)
-	find_package(OpenMP REQUIRED)
-	if(OPENMP_FOUND)
-		message("__enable_opemmp Find OpenMP.")
-		if(ANDROID)
-			message(WARNING "OpenMP_CXX_LIBRARIES:  ${OpenMP_CXX_LIBRARIES}")
-			message(WARNING "OpenMP_CXX_FLAGS:  ${OpenMP_CXX_FLAGS}")
+	if(CC_BC_WIN OR CC_BC_MAC)
+		find_package(OpenMP REQUIRED)
+		if(OPENMP_FOUND)
+			message("__enable_opemmp Find OpenMP.")
+			if(ANDROID)
+				message(WARNING "OpenMP_CXX_LIBRARIES:  ${OpenMP_CXX_LIBRARIES}")
+				message(WARNING "OpenMP_CXX_FLAGS:  ${OpenMP_CXX_FLAGS}")
+			endif()
+			if(TARGET OpenMP::OpenMP_CXX)
+				message(STATUS "OpenMP TARGET OpenMP::OpenMP_CXX Imported.")
+			endif()
 		endif()
-		if(TARGET OpenMP::OpenMP_CXX)
-			message(STATUS "OpenMP TARGET OpenMP::OpenMP_CXX Imported.")
+	
+	elseif(CC_BC_LINUX)
+		if(NOT OPENMP_ROOT)
+			set(OPENMP_ROOT /usr/lib/llvm-10) 
 		endif()
-	endif()
-elseif(CC_BC_LINUX)
-if(NOT OPENMP_ROOT)
-	set(OPENMP_ROOT usr/lib/llvm-10) 
-endif()
-
-if(NOT EXISTS ${OPENMP_ROOT}/include/openmp/omp.h)
-	message(FATAL_ERROR "OPENMP_ROOT [${OPENMP_ROOT}] error.")
-endif()
-set(OpenMP::OpenMP_CXX_INCLUDE_DIRS ${OPENMP_ROOT}/include/openmp)
-
-    find_library(OpenMP::OpenMP_CXX_LIBRARIES_DEBUG
+	
+		if(NOT EXISTS ${OPENMP_ROOT}/include/openmp/omp.h)
+			message(FATAL_ERROR "OPENMP_ROOT [${OPENMP_ROOT}] error.")
+		endif()
+		set(openmp_INCLUDE_DIRS ${OPENMP_ROOT}/include/openmp)
+	
+		find_library(openmp_LIBRARIES_DEBUG
+					NAMES omp
+					PATHS "${OPENMP_ROOT}/lib/")
+					
+		find_library(openmp_LIBRARIES_RELEASE
 				NAMES omp
 				PATHS "${OPENMP_ROOT}/lib/")
-				
-	find_library(OpenMP::OpenMP_CXX_LIBRARIES_RELEASE
-			NAMES omp
-			PATHS "${OPENMP_ROOT}/lib/")
-    set(OpenMP::OpenMP_CXX_LOC_DEBUG ${OpenMP::OpenMP_CXX_LIBRARIES_DEBUG})
-	set(OpenMP::OpenMP_CXX_RELEASE ${OpenMP::OpenMP_CXX_LIBRARIES_RELEASE})
-
-	message("OpenMP::OpenMP_CXX_INCLUDE_DIRS  ${OpenMP::OpenMP_CXX_INCLUDE_DIRS}")
-
-	message("OpenMP::OpenMP_CXX_LIBRARIES_DEBUG  ${OpenMP::OpenMP_CXX_LIBRARIES_DEBUG}")
-	message("OpenMP::OpenMP_CXX_LIBRARIES_RELEASE  ${OpenMP::OpenMP_CXX_LIBRARIES_RELEASE}")
-
-	if(OpenMP::OpenMP_CXX_INCLUDE_DIRS AND OpenMP::OpenMP_CXX_LIBRARIES_DEBUG AND OpenMP::OpenMP_CXX_LIBRARIES_RELEASE)
-		set(OpenMP::OpenMP_CXX_FOUND "True")
-		__import_target(OpenMP::OpenMP_CXX dll)
-		message("import target OpenMP::OpenMP_CXX +++++")
-	endif()
+		set(openmp_LOC_DEBUG ${openmp_LIBRARIES_DEBUG})
+		
+		set(openmp_INCLUDE_DIRS ${OPENMP_ROOT}/include/openmp)
+		
+		set(openmp_RELEASE ${openmp_LIBRARIES_RELEASE})
 	
-endif()
+		message("OpenMP::OpenMP_CXX_INCLUDE_DIRS  ${OpenMP::OpenMP_CXX_INCLUDE_DIRS}")
+	
+		message("OpenMP::OpenMP_CXX_LIBRARIES_DEBUG  ${OpenMP::OpenMP_CXX_LIBRARIES_DEBUG}")
+		message("OpenMP::OpenMP_CXX_LIBRARIES_RELEASE  ${OpenMP::OpenMP_CXX_LIBRARIES_RELEASE}")
+	
+		if(openmp_INCLUDE_DIRS AND openmp_LIBRARIES_DEBUG AND openmp_LIBRARIES_RELEASE)
+
+			set(OpenMP::OpenMP_CXX_FOUND "True")
+			__import_target(openmp dll)
+			add_library(OpenMP::OpenMP_CXX ALIAS openmp)
+			message("import target OpenMP::OpenMP_CXX +++++")
+		endif()
+		
+	endif()
 endmacro()
 
 macro(__use_openmp target)
