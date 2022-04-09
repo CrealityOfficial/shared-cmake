c6fb3460ac08ffc04cf2b2304f2b91d8a124d81e
anoob <anoob@sina.cn>
2022-04-09 14:50:04 +0800

    ninja

--- ci/test-win32.bat
+++ ci/test-win32.bat
@@ -5,6 +5,28 @@ if [%1] == [] (
 	set Generator=Visual Studio 16 2019
 )
 
+if "%1" == "Ninja" ^
+(
+
+set VSENV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+if exist %VSENV% (
+call %VSENV% 
+goto :build
+) else (echo "not find vs in" %VSENV%)
+
+set VSENV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
+if exist %VSENV% (
+call %VSENV% 
+goto :build
+) else (echo "not find vs in" %VSENV%)
+
+set VSENV="D:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+if exist %VSENV% (call %VSENV% ) else (exit /B)
+
+)
+else ^
+(
+
 call "%~dp0\find-msvcinfo.bat"
 if not %errorlevel%==0 (exit /b 1)
 		
@@ -13,6 +35,7 @@ call "%VSDir%\VC\Auxiliary\Build\vcvars64.bat"
 REM 从注册表查询 win10 SDK 安装目录，主要用于判断注册表查询是否被禁，如果被禁 vcvars64.bat 将无法正常配置环境，需要手动配置环境依赖
 call "%~dp0\check-vcvars64.bat"
 
+)
 :build
 echo "build"
 
