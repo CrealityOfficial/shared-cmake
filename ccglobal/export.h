7ff05152430fd9846416f19bc86bb9811540532b
anoob <anoob@sina.cn>
2022-04-24 16:13:45 +0800

    export

--- ccglobal/export.h
+++ ccglobal/export.h
@@ -2,10 +2,19 @@
 #define EXPORT_INTERFACE_1604911737496_H
 
 #ifdef WIN32
-	#define CC_DECLARE_EXPORT __declspec(dllexport)
-	#define CC_DECLARE_IMPORT __declspec(dllimport)
+	#if CC_FORCE_EXPORT_STATIC
+		#define CC_DECLARE_EXPORT 
+		#define CC_DECLARE_IMPORT
+	#elif CC_FORCE_IMPORT_STATIC
+		#define CC_DECLARE_EXPORT __declspec(dllexport) 
+		#define CC_DECLARE_IMPORT
+	#else
+		#define CC_DECLARE_EXPORT __declspec(dllexport)
+		#define CC_DECLARE_IMPORT __declspec(dllimport)
+	#endif
 #else
 	#define CC_DECLARE_EXPORT __attribute__((visibility("default")))
 	#define CC_DECLARE_IMPORT
 #endif
+
 #endif // EXPORT_INTERFACE_1604911737496_H
