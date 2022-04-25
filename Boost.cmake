ec68bb38c7bb4ecf17918ad4536f9169265478ea
anoob <anoob@sina.cn>
2022-04-25 18:14:29 +0800

    boost

--- Boost.cmake
+++ Boost.cmake
@@ -2,10 +2,29 @@ macro(__add_boost_target module)
 	__source_recurse(${CMAKE_CURRENT_SOURCE_DIR} SRC)
 	__source_recurse(${boost_includes}boost/${module}/ HEADER)
 	
-	__add_real_target(boost_${module} dll SOURCE ${SRC} ${HEADER}
-										  INC ${CMAKE_CURRENT_SOURCE_DIR}/../../
-										  DEF BOOST_ALL_DYN_LINK
-										  INTERFACE ${CMAKE_CURRENT_SOURCE_DIR}/../../)
+	set(btarget boost_${module})
+	string(TOUPPER ${btarget} UpperName)
+			
+	set(INCS ${CMAKE_CURRENT_SOURCE_DIR}/../../)
+	set(INTERFACES ${CMAKE_CURRENT_SOURCE_DIR}/../../)
+	set(SRCS ${SRC} ${HEADER})
+	if(CC_GLOBAL_FORCE_STATIC OR ${UpperName}_STATIC)
+		__add_real_target(${btarget} lib SOURCE ${SRCS} 
+										LIB ${LIBS}
+										INC ${INCS}
+										DEF ${DEFS}
+										INTERFACE ${INTERFACES}
+										)
+	else()
+		list(APPEND DEFS BOOST_ALL_DYN_LINK)
+		__add_real_target(${btarget} dll SOURCE ${SRCS} 
+										LIB ${LIBS}
+										INC ${INCS}
+										DEF ${DEFS}
+										INTERFACE ${INTERFACES}
+										)
+		set_property(TARGET ${btarget} PROPERTY INTERFACE_COMPILE_DEFINITIONS BOOST_ALL_DYN_LINK)
+	endif()
 endmacro()
 
 macro(__find_boost_root)
