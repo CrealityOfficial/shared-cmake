2519414cde3b876b165e0532122c20ad5474c4eb
suruochang <suruochang@creality.com>
2022-05-07 15:55:39 +0800

    新增iOS _cc_access 函数定义
    Change-Id: Iccfa6ea43e91efd4d55fc83b1b574dbadf4f7515

--- ccglobal/platform.h
+++ ccglobal/platform.h
@@ -2,20 +2,26 @@
 #define __CCGLOBAL_PLATFORM_H
 
 #include <stdio.h>
+
 #if CC_SYSTEM_WIN
 #include <io.h>
 #define _cc_access access
+
 #elif CC_SYSTEM_ANDROID
 #include <unistd.h>
 #define _cc_access access
+
 #elif CC_SYSTEM_LINUX
 #include <unistd.h>
 #define _cc_access access
-#elif CC_SYSTEM_MAC
+
+#elif CC_SYSTEM_MAC || CC_SYSTEM_IOS
 #include <unistd.h>
 #define _cc_access access
+
 #else
 #include <unistd.h>
 #define _cc_access access
 #endif
+
 #endif // __CCGLOBAL_PLATFORM_H
