3c2a0fe8f0a01936fbae9b1746afaa11f09295e5
anoob <anoob@sina.cn>
2022-05-11 14:53:41 +0800

    platform

--- ccglobal/platform.h
+++ ccglobal/platform.h
@@ -2,31 +2,23 @@
 #define __CCGLOBAL_PLATFORM_H
 
 #include <stdio.h>
+#include <cmath>
 
 #if CC_SYSTEM_WIN
 #include <io.h>
-#define _cc_access access
-
-#elif CC_SYSTEM_ANDROID
-#include <unistd.h>
-#include <cmath>
-#define _cc_access access
-
-#elif CC_SYSTEM_LINUX
-#include <unistd.h>
-#include <cmath>
-#define _cc_access access
-
-#elif CC_SYSTEM_MAC || CC_SYSTEM_IOS
-#include <unistd.h>
-#include <cmath>
-#define _cc_access access
-
 #else
 #include <unistd.h>
-#include <cmath>
+#endif
 #define _cc_access access
 
+#if CC_SYSTEM_WIN
+	#define _cc_ftelli64 _ftelli64
+#else
+	#ifdef __USE_FILE_OFFSET64
+		#define _cc_ftelli64 ftello64
+	#else
+		#define _cc_ftelli64 ftell
+	#endif
 #endif
 
 #endif // __CCGLOBAL_PLATFORM_H
