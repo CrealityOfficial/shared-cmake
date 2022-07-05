285c84d7cb3fcd6bef27b9d11cec5ddfbd192dec
anoob <anoob@sina.cn>
2022-07-05 10:41:37 +0800

    feature:[conan]find_conan_trimesh2

--- conan/Find_conan_trimesh2.cmake
+++ conan/Find_conan_trimesh2.cmake
@@ -1,14 +1,3 @@
 # trimesh2 target
 
-if(NOT TARGET trimesh2)
-	__assert_parameter(CONAN_TRIMESH2_ROOT_RELEASE)
-	set(trimesh2_INCLUDE_DIRS ${CONAN_TRIMESH2_ROOT_RELEASE}/include/)
-	
-    if(CC_BC_WIN)
-		set(trimesh2_LIBRARIES_DEBUG "${CONAN_TRIMESH2_ROOT_DEBUG}/lib/Debug/trimesh2.lib")
-	    set(trimesh2_LIBRARIES_RELEASE "${CONAN_TRIMESH2_ROOT_RELEASE}/lib/Release/trimesh2.lib")
-	elseif(CC_BC_LINUX)	
-	endif()
-	
-	__test_import(trimesh2 lib)
-endif()
+__conan_import(trimesh2 lib)
