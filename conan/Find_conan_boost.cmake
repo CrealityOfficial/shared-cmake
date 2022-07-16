b5a40e259d6039cf6467d92bf97874767b5f03a6
hemiao <hemiao@creality.com>
2022-07-16 16:43:31 +0800

    chore:[boost]add boost static

--- conan/Find_conan_boost.cmake
+++ conan/Find_conan_boost.cmake
@@ -1,9 +1,19 @@
 # boost target
 
-__conan_import(boost dll COMPONENT boost_file_system
-								   boost_iostreams
-								   boost_nowide
-								   boost_regex
-								   boost_system
-								   boost_thread
-								   )
+if(BOOST_STATIC)
+	__conan_import(boost lib COMPONENT boost_filesystem
+									boost_iostreams
+									boost_nowide
+									boost_regex
+									boost_system
+									boost_thread
+									)
+else()
+	__conan_import(boost dll COMPONENT boost_file_system
+									boost_iostreams
+									boost_nowide
+									boost_regex
+									boost_system
+									boost_thread
+									)
+endif()
