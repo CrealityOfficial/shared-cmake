67ac7fc73254dacb4e2fc4ce3f9425c5deb04a8b
wangwenbin131416 <wangwenbin@creality.com>
2022-07-28 11:56:56 +0800

    chore[cmake]conan freetype find
    Change-Id: I51feb5a6a2e46032ffd2146f6b6a73a7c4d2dbb5

--- conan/Find_conan_freetype.cmake
+++ conan/Find_conan_freetype.cmake
@@ -1,5 +1,5 @@
 # freetype target
 if(CONAN_FREETYPE_ROOT_RELEASE)
-	set(FREETYPE_INCLUDE_DIRS "${CONAN_FREETYPE_ROOT_RELEASE}/include/")
+	set(FREETYPE_INCLUDE_DIRS "${CONAN_FREETYPE_ROOT_RELEASE}/include/freetype2/")
 endif()
 __conan_import(freetype lib)
