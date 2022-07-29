e682f51f60911f1f9e9c3ba71d25b779f91d735c
wangwenbin131416 <wangwenbin@creality.com>
2022-07-29 11:55:52 +0800

    chore[openssl]conan _cc_find update
    Change-Id: Ic25df2c9182a2126ba84279205ab8c7459911c5c

--- conan/Find_conan_openssl.cmake
+++ conan/Find_conan_openssl.cmake
@@ -1,5 +1,5 @@
 if(CONAN_OPENSSL_ROOT_RELEASE)
-	set(OPENSSL_INCLUDE_DIR ${CONAN_OPENSSL_ROOT_RELEASE}/include/openssl/)
+	set(OPENSSL_INCLUDE_DIR ${CONAN_OPENSSL_ROOT_RELEASE}/include/openssl/ ${CONAN_OPENSSL_ROOT_RELEASE}/include/openssl/openssl/ ${CONAN_OPENSSL_ROOT_RELEASE}/include/openssl/crypto/ ${CONAN_OPENSSL_ROOT_RELEASE}/include/openssl/internal/)
 endif()
 
 __conan_import(openssl dll COMPONENT ssl crypto)
