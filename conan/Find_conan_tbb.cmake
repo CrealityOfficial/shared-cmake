7c570aedc21e69120e6e56688d2e5a15da3bc914
hemiao <hemiao@creality.com>
2022-08-17 19:54:23 +0800

    ci:[conan]add conan ci for emcc

--- conan/Find_conan_tbb.cmake
+++ conan/Find_conan_tbb.cmake
@@ -1,7 +1,10 @@
 # tbb target
-
+if(CC_BC_EMCC)
+	__conan_import(tbb lib)
+else()
 __conan_import_one(tbb dll NAME tbb
 						   LIB  tbb
 						   DLL  tbb
 						   INTERFACE_DEF __TBB_NO_IMPLICIT_LINKAGE
 						   )
+endif()
