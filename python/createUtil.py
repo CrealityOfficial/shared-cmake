b148d844794e522c7dc5c562c33a3d1f98fe2b15
anoob <anoob@sina.cn>
2022-08-25 11:22:50 +0800

    feature:[python]scripts

---
+++ python/createUtil.py
@@ -0,0 +1,138 @@
+import os
+import sys, getopt
+import tempfile
+import shutil
+from xml.etree import ElementTree as ET
+
+def toDotID(name, version):
+    v = version.replace('.', '_')
+    n = name + "_" + v
+    return n
+    
+def toDotName(name, version):
+    n = name + "." + version
+    return n     
+    
+def create_sub_libs():
+    name = ''
+    version = ''
+    directory = sys.path[0]
+    argv = sys.argv[1:]
+    xml_file = directory + "/cmake/conan/graph/libs.xml"
+    
+    try:
+        opts, args = getopt.getopt(argv,"n:v:p:")
+    except getopt.GetoptError:
+        print("create.py -n <name>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-n"):
+            name = arg
+        elif opt in ("-v"):
+            version = arg
+    
+    return create_sub_libs_from_xml(xml_file, name, version)
+    
+       
+def create_sub_libs_from_xml(xml_file, name, version):
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    ref = name + '/' + version
+    
+    libs = root.findall("lib")
+    subs = {}
+    for lib in libs:
+        sublibs = lib.findall("sublib")
+        subss = []
+        for sub in sublibs:
+            subss.append(sub.attrib["name"])
+        
+        subs[lib.attrib["name"]] = subss
+        
+    result = []
+    
+    first = subs[ref];
+    second = []
+    while len(first) > 0:
+        for value in first:
+            if value not in result:
+                result.append(value)
+                nex = subs[value]
+                for nvalue in  nex:
+                    if nvalue not in second:
+                        second.append(nvalue)
+            
+        first = second
+        second = []
+    
+    return result      
+       
+def create_param_from_argv():
+    directory = sys.path[0]
+    argv = sys.argv[1:]
+    print("[conan DEBUG] file path " + directory)
+    print("[conan DEBUG] input argv " + str(argv))
+    
+    name = '' 
+    version = ''
+    profile = 'default'
+    user_channel = '_/_'
+    try:
+        opts, args = getopt.getopt(argv,"n:v:p:")
+    except getopt.GetoptError:
+        print("create.py -n <name> -v <version> -p <profile>")
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-n"):
+            name = arg
+        elif opt in ("-v"):
+            version = arg
+        elif opt in ("-p"):
+            profile = arg
+    
+    params = {'name':name, 'version':version, 'profile':profile, 'channel':user_channel}
+    return params
+    
+def invoke_conan_build(params, subLibs = []):
+    print("[conan DEBUG] params : " + str(params))
+    print("[conan DEBUG] subs : " + str(subLibs))
+    
+    directory = sys.path[0]
+    name = params['name']
+    profile = params['profile']
+    version = params['version']
+    user_channel = params['channel']
+    
+    with tempfile.TemporaryDirectory() as temp_directory:
+        print("[conan DEBUG] created temporary directory ", temp_directory)
+        print("[conan DEBUG] temp directory exist ", os.path.exists(temp_directory))
+        
+        create_script_src = directory + "/scripts/conanfile.py"
+        cmake_script_src = directory + "/scripts/CMakeLists.txt"
+        meta_data_src = directory + "/recipes/" + name + "/conandata.yml";
+        shutil.copy2(create_script_src, temp_directory)
+        shutil.copy2(meta_data_src, temp_directory)
+        shutil.copy2(cmake_script_src, temp_directory)
+        
+        meta_data_dest = temp_directory + "/conandata.yml"
+        meta_file = open(meta_data_dest, "a")
+        meta_file.write("version: " + "\"" + version + "\"\n")
+        meta_file.write("name: " + "\"" + name + "\"\n")
+        meta_file.write("channel: " + "\"" + user_channel + "\"")
+        meta_file.close()
+        cmake_script_dest = temp_directory + "/CMakeLists.txt"
+        cmake_file = open(cmake_script_dest, "a")
+        cmake_file.write("add_subdirectory(" + name + ")")
+        cmake_file.close()       
+        sublibs_dest = temp_directory + "/requires.txt"
+        subLibs_file = open(sublibs_dest, "w")
+        for sub in subLibs:
+            subLibs_file.write(sub)
+            subLibs_file.write('\n')
+        subLibs_file.close() 
+        
+        #os.system("pause")
+        debug_cmd = 'conan create --profile ' + profile + ' -s build_type=Debug ' + temp_directory + ' ' + user_channel
+        os.system(debug_cmd)
+        release_cmd = 'conan create --profile ' + profile + ' -s build_type=Release ' + temp_directory + ' ' + user_channel        
+        os.system(release_cmd)
