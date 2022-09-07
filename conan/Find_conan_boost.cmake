6feb73381888b178fb0e46b143207b39bff3731b
zenggui <anoob@sina.cn>
2022-09-07 23:46:22 +0800

    feature:[libs]find_conan_boost

--- conan/Find_conan_boost.cmake
+++ conan/Find_conan_boost.cmake
@@ -3,20 +3,22 @@ if(CONAN_BOOST_ROOT_RELEASE)
 	set(BOOST_INCLUDE_DIRS ${CONAN_BOOST_ROOT_RELEASE}/include/)
 endif()
 
-if(BOOST_STATIC)
-	__conan_import(boost lib COMPONENT boost_filesystem
-									boost_iostreams
-									boost_nowide
-									boost_regex
-									boost_system
-									boost_thread
-									)
-else()
-	__conan_import(boost dll COMPONENT boost_file_system
-									boost_iostreams
-									boost_nowide
-									boost_regex
-									boost_system
-									boost_thread
-									)
+set(BOOST_COMPONETS boost_filesystem
+					boost_iostreams
+					boost_nowide
+					boost_regex
+					boost_system
+					boost_thread
+					)
+					
+if(CC_BC_WIN)
+	list(APPEND BOOST_COMPONETS boost_log)
 endif()
+
+if(BOOST_STATIC)
+	__conan_import(boost lib COMPONENT ${BOOST_COMPONETS})
+else()
+	__conan_import(boost dll COMPONENT ${BOOST_COMPONETS})
+endif()
+
+__conan_import(boost lib COMPONENT boost_test)
