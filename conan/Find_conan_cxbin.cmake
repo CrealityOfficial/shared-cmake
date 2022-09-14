2cfd893d2dd0d7e712347c95a32b183cdbd82f59
wangjiang <wangjiang@creality.com>
2022-09-14 09:47:46 +0800

    fix:[boost_systemfile]更改库名
    Change-Id: Idb50fdb2ece86bb1aeec2650837d11326a14b506

--- conan/Find_conan_cxbin.cmake
+++ conan/Find_conan_cxbin.cmake
@@ -4,6 +4,6 @@ __conan_find(zip)
 __conan_find(colladadom)
 __conan_find(stringutil)
 
-__conan_import(boost dll COMPONENT boost_file_system)
+__conan_import(boost dll COMPONENT boost_filesystem)
 
 __conan_import(cxbin dll)
