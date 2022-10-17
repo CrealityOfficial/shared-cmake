5c8be55ef683fc74b81c42acc4d9562882c291f9
anoob <anoob@sina.cn>
2022-10-17 18:05:35 +0800

    feature:[conan]cmesh fmesh splitslot

--- conan/Find_conan_fmesh.cmake
+++ conan/Find_conan_fmesh.cmake
@@ -1,6 +1,7 @@
 # fmesh target
-__conan_import(dxflib dll)
-__conan_import(librevenge dll)
-__conan_import(freetype dll)
-__conan_import(cmesh dll)
+__cc_find(dxflib)
+__cc_find(librevenge)
+__cc_find(freetype)
+__cc_find(cmesh)
+
 __conan_import(fmesh dll)
