f452c68dd48a8d71cbbc3b093b4e8fc6faf6eb33
liuhong <liuhong@creality.com>
2022-10-27 19:22:33 +0800

    fix[build]:add library for mac

--- conan/Find_conan_mpfr.cmake
+++ conan/Find_conan_mpfr.cmake
@@ -1,5 +1,8 @@
 # mpfr target
-
-__conan_import_one(mpfr dll NAME mpfr LIB libmpfr-4 DLIB libmpfr-4
+if(CC_BC_WIN)
+	__conan_import_one(mpfr dll NAME mpfr LIB libmpfr-4 DLIB libmpfr-4
 									  DLL libmpfr-4 DDLL libmpfr-4
 									  )
+else()
+	__conan_import(mpfr lib)
+endif()
