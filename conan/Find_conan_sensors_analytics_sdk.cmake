cfebb139f7622586ed775fa9cd1be33bb0271a80
anoob <anoob@sina.cn>
2022-12-07 19:56:21 +0800

    feature:[sdk]lib

--- conan/Find_conan_sensors_analytics_sdk.cmake
+++ conan/Find_conan_sensors_analytics_sdk.cmake
@@ -1,6 +1,6 @@
 #sensors_analytics_sdk target
 if(CC_BC_MAC)
-__conan_import(sensors_analytics_sdk lib)
+	__conan_import(sensors_analytics_sdk lib)
 else()
-__conan_import(sensors_analytics_sdk dll)
+	__conan_import(sensors_analytics_sdk lib)
 endif()
