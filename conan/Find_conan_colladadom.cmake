4a557142607e35c73dcd2fb192e6f41501d05edf
wangjiang <wangjiang@creality.com>
2023-03-18 16:58:53 +0800

    fix colladadom linux build

--- conan/Find_conan_colladadom.cmake
+++ conan/Find_conan_colladadom.cmake
@@ -1,6 +1,8 @@
 # colladadom target
 if(CC_BC_EMCC)
 __conan_import(colladadom lib COMPONENT colladadom colladadom141)
+else if(CC_BC_LINUX)
+__conan_import(colladadom lib COMPONENT colladadom colladadom141)
 else()
 __conan_import(colladadom dll COMPONENT colladadom colladadom141)
 endif()
