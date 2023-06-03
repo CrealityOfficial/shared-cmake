6346c4eaba6f23350de044e4526521ae10461845
anoob <anoob@sina.cn>
2023-06-03 15:36:41 +0800

    feature:[check]path

--- ci/algrithm-auto-check.sh
+++ ci/algrithm-auto-check.sh
@@ -3,10 +3,18 @@
 PATH=$PATH:/home/cxsw/.local/bin/
 
 origin_path=$PWD
+root_path=$PWD/../
+script_path=$PWD/../auto_scripts
+
+echo "print path:"
 echo $origin_path
+echo $root_path
+echo $script_path
 
 python cmake/ci/conan-cmake.py -t linux
 cd linux-build/build/
 ninja all
-cd ../../
+cd $origin_path
+
+
 
