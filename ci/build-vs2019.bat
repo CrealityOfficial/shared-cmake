3e431af2155259234e61e5f8a305610016527c51
fanjunhong <fanjunhong@piocreat3d.com>
2023-08-16 10:48:05 +0800

    feautre:[package] 更新打包脚本
    Change-Id: I3d68e6490acd2aa9a05458e2c07324f7f60bccb3

--- ci/build-vs2019.bat
+++ ci/build-vs2019.bat
@@ -2,68 +2,76 @@
 if [%1] == [] (
 	echo "build Usage:"
 	echo "Only build: build.bat v0.1.0.1"
-	echo "Build and package:build.bat v0.1.0.1 package Alpha 0"
+	echo "Build and package:build.bat v0.1.0.1 package Alpha 0 Creality_Print"
 	exit /b 0
 )
+
 if [%3] == [] (
 	set VERSION_EXTRA="test"
-	
 ) else (
-	set VERSION_EXTRA=%3%
+	set VERSION_EXTRA=%3
+)
+
+if "%4" == "0" (
+	set SIGN_PACKAGE="OFF"
+) else (
 	set SIGN_PACKAGE="ON"
 )
-if "%VERSION_EXTRA%" == "test" (
-	set SIGN_PACKAGE="OFF"
-)
-set FACTORY_TYPE=%4
-if [%4] == [] (
-	set FACTORY_TYPE=0
-)
-set APPNAME=%5
+
 if [%5] == [] (
 	set APPNAME="Creality Print"
+) else (
+  set APPNAME=%5
 )
+
+if [%6] == [] (
+	set CUSTOM_TYPE=""
+) else (
+  set CUSTOM_TYPE=%6
+)
+
 set APP_VER=%1
 set APP_VER=%APP_VER:~1%
 for /f "tokens=1,* delims=." %%a in ("%APP_VER%") do (
-set MAJOR=%%a
-set APP_VER=%%b
-echo %MAJOR%
+  set MAJOR=%%a
+  set APP_VER=%%b
 )
+
 for /f "tokens=1,* delims=." %%a in ("%APP_VER%") do (
-set MINOR=%%a
-set APP_VER=%%b
-echo %MINOR%
+  set MINOR=%%a
+  set APP_VER=%%b
 )
+
 for /f "tokens=1,* delims=." %%a in ("%APP_VER%") do (
-set PATCH=%%a
-set APP_VER=%%b
-echo %PATCH%
+  set PATCH=%%a
+  set APP_VER=%%b
 )
+
 for /f "tokens=1,* delims=." %%a in ("%APP_VER%") do (
-set BUILD=%%a
-set APP_VER=%%b
-echo %BUILD%
+  set BUILD=%%a
+  set APP_VER=%%b
 )
-echo %MAJOR%
-echo %MINOR%
-echo %PATCH%
-echo %BUILD%
-echo %VERSION_EXTRA%
-echo %FACTORY_TYPE%
-echo %EXTRA_OPTION%
+
+echo MAJOR=%MAJOR%
+echo MINOR=%MINOR%
+echo PATCH=%PATCH%
+echo BUILD=%BUILD%
+echo VERSION_EXTRA=%VERSION_EXTRA%
+echo APPNAME=%APPNAME%
+echo CUSTOM_TYPE=%CUSTOM_TYPE%
+echo SIGN_PACKAGE=%SIGN_PACKAGE%
 rem Configure the application in the current directory
 
 set VSENV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 if exist %VSENV% (
-call %VSENV% 
-goto :build
+  call %VSENV%
+  goto :build
 ) else (echo "not find vs in" %VSENV%)
 
 set VSENV="C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 if exist %VSENV% (
-call %VSENV% 
-goto :build
+  call %VSENV%
+  goto :build
 ) else (echo "not find vs in" %VSENV%)
 
 set VSENV="D:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
@@ -72,25 +80,24 @@ if exist %VSENV% (call %VSENV% ) else (exit /B)
 :build
 cd build
 cmake ^
-    -G"Ninja" ^
-    -DCMAKE_BUILD_TYPE=Release ^
-    -DCMAKE_INSTALL_PREFIX=out ^
-	-DPROJECT_VERSION_MAJOR=%MAJOR% ^
-	-DPROJECT_VERSION_MINOR=%MINOR% ^
-	-DPROJECT_VERSION_PATCH=%PATCH% ^
-	-DPROJECT_BUILD_ID=%BUILD% ^
-	-DPROJECT_VERSION_EXTRA=%VERSION_EXTRA% ^
-	-DFACTORY_TYPE=%FACTORY_TYPE% ^
-	-DAPPNAME=%APPNAME% ^
-	-DSIGN_PACKAGE=%SIGN_PACKAGE% ^
-	%EXTRA_OPTION% ^
-    ..\ || exit /b 2
+  -G"Ninja" ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_INSTALL_PREFIX=out ^
+  -DPROJECT_VERSION_MAJOR=%MAJOR% ^
+  -DPROJECT_VERSION_MINOR=%MINOR% ^
+  -DPROJECT_VERSION_PATCH=%PATCH% ^
+  -DPROJECT_BUILD_ID=%BUILD% ^
+  -DPROJECT_VERSION_EXTRA=%VERSION_EXTRA% ^
+  -DAPPNAME=%APPNAME% ^
+  -DCUSTOM_TYPE=%CUSTOM_TYPE% ^
+  -DSIGN_PACKAGE=%SIGN_PACKAGE% ^
+  ..\ || exit /b 2
 
 rem Build and install the application
 
 ninja || exit /b 2
 if "%2"=="package" (
-ninja install || exit /b 2
-ninja package || exit /b 2
+  ninja install || exit /b 2
+  ninja package || exit /b 2
 )
 cd ..\
--- ci/jenkins-beta-build.bat
+++ ci/jenkins-beta-build.bat
@@ -2,11 +2,13 @@
 set APP_NAME=%1
 set TAG_NAME=%2
 set BUILD_TYPE=%3
-set FACTORY_TYPE=%4
+set SIGIN=%4
+set CUSTOM_TYPE=%5
 
 echo "build"
 rem rd /s /q build
 if not exist build md build
+if exist build\CMakeCache.txt del build\CMakeCache.txt
 python cmake\ci\conan-cmake.py -t jwin
 
 git show-ref %TAG_NAME% | awk -F ' ' '{print $1}' >cmmid
@@ -17,10 +19,9 @@ set /p MAXCMMID=<maxcmmid
 set /p TAGCMMID=<tagcmmid
 set /a TAGNUMB=%MAXCMMID%-%TAGCMMID%
 set TAG_NAME=%TAG_NAME%.%TAGNUMB%
-call %~dp0\build-vs2019.bat %TAG_NAME% package %BUILD_TYPE% %FACTORY_TYPE% %APP_NAME% || exit /b 2
+call %~dp0\build-vs2019.bat %TAG_NAME% package %BUILD_TYPE% %SIGIN% %APP_NAME% %CUSTOM_TYPE% || exit /b 2
 set EXE_NAME=%APP_NAME%-%TAG_NAME%-win64-%BUILD_TYPE%.exe
 echo "build"
-	
 
 echo SIGN_PACKAGE_PATH=%JOB_NAME%> var.prop
 echo SIGN_PACKAGE_NAME=%EXE_NAME%>> var.prop
@@ -28,5 +29,3 @@ mkdir %JOB_NAME%
 scp -r %JOB_NAME% cxsw@172.16.33.10:/vagrant_data/www/shared/build
 cd build
 scp %EXE_NAME% cxsw@172.16.33.10:/vagrant_data/www/shared/build/%JOB_NAME%/
-	
-
