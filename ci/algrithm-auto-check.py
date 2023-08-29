0ec08b539efe35610be3cd45ccad94a9d69a1eef
zenggui <anoob@sina.cn>
2023-08-30 00:18:44 +0800

    1

--- ci/algrithm-auto-check.py
+++ ci/algrithm-auto-check.py
@@ -3,27 +3,14 @@ import sys;
 if __name__ == "__main__":
     directory = sys.path[0]
     sys.path.append(sys.path[0] + '/../python/')
-
-    import osSystem;
-
+    
     origin_path = sys.path[0] + '/../../'
-    script_path = origin_path + 'auto_scripts/'
+    print("algrithm auto check")
+    print("origin_path: " + origin_path)
     
-    print('AUTO CHECK ....')
-    print('origin_path : ' + origin_path)
-    print('script_path : ' + script_path)
+    import origin_path + '/demo/test.py'
     
-    autogen_path = origin_path + 'autocheck_generate'
-    osSystem.mkdirs(autogen_path)
-    
-    import ReadWrite;
-    print('doxygen ....')
-    doxygen_config = autogen_path 
-    user_config_file = script_path + 'doxygen.cfg'
-    user_items = ReadWrite.parse_items(user_config_file, 'default')
-    
-    for key in user_items:  
-        print(key + " : " + user_items[key])
+
     
     
     
--- ci/algrithm-auto-check.sh
+++ ci/algrithm-auto-check.sh
@@ -28,22 +28,4 @@ cd $origin_path
 #test
 pwd
 echo "run test"
-bin_path=$origin_path/linux-build/bin/Release/
-$bin_path/unit_test_format 
-
-#web hook
-echo "web hook"
-string1=$job_display_name
-string2=$job_base_name
-
-sfiless=$origin_path$/linux-build/bin/Release/out.csv
-chmod -R 777 $sfiless
-
-cont=$(cat $sfiless|sed ':label;N;s/\n/\\n/;b label')
-cont=${cont//,/------------->} 
-
-
-curl -X POST -H "Content-Type: application/json" \
--d '{"msg_type":"post","content": {"post": {"zh_cn": {"title": "自动测试结果通知","content": [[{"tag": "text","text": "'"项目名称：$string2\n构建编号：第$BUILD_NUMBER次构建\n远程分支：$GIT_BRANCH\n构建状态：成功\n构建日期：$nowTime\n输出结果：\n$cont"'"}]]} } }}' \
-https://open.feishu.cn/open-apis/bot/v2/hook/7ffe1550-8904-4b09-9e59-b3aaf4224236
-
+python3 cmake/ci/algrithm-auto-check.py
