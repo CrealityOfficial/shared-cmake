adf206c7226a5c6bc84894453a8b9a2671debd31
zenggui <anoob@sina.cn>
2023-08-30 00:35:29 +0800

    1

--- ci/algrithm-auto-check.py
+++ ci/algrithm-auto-check.py
@@ -5,9 +5,16 @@ if __name__ == "__main__":
     sys.path.append(sys.path[0] + '/../python/')
     
     origin_path = sys.path[0] + '/../../'
+    bin_path = origin_path + 'linux-build/bin/Release/'
+    
     print("algrithm auto check")
     print("origin_path: " + origin_path)
     
+    import osSystem
+    def execute_test(name):
+        cmd = bin_path + name
+        osSystem.system(cmd)
+        
     sys.path.append(origin_path)
     import demo.test
     
