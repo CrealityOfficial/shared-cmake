f429a014827226877db26c1cda0be2743a293b07
zenggui <anoob@sina.cn>
2023-08-30 02:22:24 +0800

    1

--- python/autoTest.py
+++ python/autoTest.py
@@ -1,21 +1,128 @@
 import sys
 import osSystem
+import json
+import requests
 
 origin_path = sys.path[0] + '/../../'
 bin_path = origin_path + 'linux-build/bin/Release/'
 webhook = sys.argv[1]
 
-def execute_hook(json):
-    cmd = 'curl -X POST -H "Content-Type: application/json" -d ' + json + ' ' + webhook
-    osSystem.system(cmd)
+def send_text(Text):
+    """发送普通消息"""
+    url = webhook
+    headers = {"Content-Type": "text/plain"}
+    data = {
+        "msg_type": "text",
+        "content": {
+            "text": Text
+        }
+    } 
+    r = requests.post(url, headers=headers, json=data)
+    return r.text
+
+def send_markdown(text):
+    """发送富文本消息"""
+    url = webhook
+    headers = {"Content-Type": "text/plain"}
+    data = {
+        "msg_type": "interactive",
+        "card": {
+  "config": {
+    "wide_screen_mode": True
+  },
+  "header": {
+    "title": {
+      "tag": "plain_text",
+      "content": "你有一个休假申请待审批"
+    }
+  },
+  "elements": [
+    {
+      "tag": "div",
+      "fields": [
+        {
+          "is_short": True,
+          "text": {
+            "tag": "lark_md",
+            "content": "**申请人**\n王晓磊"
+          }
+        },
+        {
+          "is_short": True,
+          "text": {
+            "tag": "lark_md",
+            "content": "**休假类型：**\n年假"
+          }
+        },
+        {
+          "is_short": False,
+          "text": {
+            "tag": "lark_md",
+            "content": ""
+          }
+        },
+        {
+          "is_short": False,
+          "text": {
+            "tag": "lark_md",
+            "content": "**时间：**\n2020-4-8 至 2020-4-10（共3天）"
+          }
+        },
+        {
+          "is_short": False,
+          "text": {
+            "tag": "lark_md",
+            "content": ""
+          }
+        },
+        {
+          "is_short": True,
+          "text": {
+            "tag": "lark_md",
+            "content": "**备注**\n因家中有急事，需往返老家，故请假"
+          }
+        }
+      ]
+    },
+    {
+      "tag": "note",
+      "elements": [
+        {
+          "tag": "img",
+          "img_key": "img_b79d3e29-5ffe-4897-822a-9743fe9f7b1g",
+          "alt": {
+            "tag": "plain_text",
+            "content": "图片"
+          }
+        },
+        {
+          "tag": "plain_text",
+          "content": "已同意休假申请"
+        }
+      ]
+    }
+  ]
+        }
+    } 
+    r = requests.post(url, headers=headers, json=data)
+    return r.text
+
+def send_card(Text):
+    """发送卡片消息"""
+    url = webhook
+    headers = {"Content-Type": "text/plain"}
+    data = {
+        "msg_type": "interactive",
+        "card": Text
+    } 
+    r = requests.post(url, headers=headers, json=data)
+    return r.text
     
 def execute_test(name):
     cmd = bin_path + name
     osSystem.system(cmd)
-    
-    json = '{"msg_type":"post","content": {"post": {"zh_cn": {"title": "自动测试结果通知","content": [[{"tag": "text","text": "'"项目名称：构建编号：第次构建\n远程分支：\n构建状态：成功\n构建日期：\n输出结果：\n"'"}]]} } }}'
-    execute_hook(json)
-    
+   
+    send_markdown("hello")  
     
 #web hook
 #echo "web hook"
