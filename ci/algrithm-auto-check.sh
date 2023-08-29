91672c592727282d97d96dfa1ffe1c724a8afed9
anoob <anoob@sina.cn>
2023-08-29 19:49:24 +0800

    feature:[1]

--- ci/algrithm-auto-check.sh
+++ ci/algrithm-auto-check.sh
@@ -3,23 +3,18 @@
 PATH=$PATH:/home/cxsw/.local/bin/
 
 origin_path=$PWD
-script_path=$PWD/auto_scripts/
 
 #path
 echo "print path:"
 echo $origin_path
-echo $script_path
-
-#doxygen
-
-
-
 
 #compile
+echo "compile"
 python cmake/ci/conan-cmake.py -t linux
 cd linux-build/build/
 ninja all
 cd $origin_path
 
-
+#test
+echo "test"
 
