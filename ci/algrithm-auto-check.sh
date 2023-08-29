7d80961f912b2617a1fdd3b83a9a62544cea5ed1
zenggui <anoob@sina.cn>
2023-08-29 23:29:54 +0800

    1

--- ci/algrithm-auto-check.sh
+++ ci/algrithm-auto-check.sh
@@ -8,6 +8,7 @@ job_url="${JENKINS_URL}job/${JOB_NAME}"
 job_display_name=$BUILD_DISPLAY_NAME
 job_base_name=$JOB_BASE_NAME
 now_time=$(date "+%Y-%m-%d %H:%M:%S")
+git_branch=$GIT_BRANCH
 
 echo "jeckin env"
 echo "origin_path" $origin_path
@@ -15,7 +16,7 @@ echo "job_url" $job_url
 echo "job_display_name" $job_display_name
 echo "now_time" $now_time
 echo "job_base_name" $job_base_name
-
+echo "git_branch" $git_branch
 
 #compile
 echo "compile"
