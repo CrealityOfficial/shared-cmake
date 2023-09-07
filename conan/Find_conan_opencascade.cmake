fc079983bb13853df0811238ba2fb802d0300bf4
anoob <anoob@sina.cn>
2023-09-07 14:56:29 +0800

    feature:find opencascade

--- conan/Find_conan_opencascade.cmake
+++ conan/Find_conan_opencascade.cmake
@@ -1,99 +1,48 @@
 
 
-set(OPENCASCADE_COMPONETS TKBin
-						  TKBinL
-						  TKBinTObj
-						  TKBinXCAF
-						  TKBO
-						  TKBool
-						  TKBRep
-						  TKCAF
-						  TKCDF
-						  TKD3DHost
-						  TKD3DHostTest
-						  TKDCAF
-						  TKDFBrowser
-						  TKDraw
-						  TKernel
-						  TKExpress
-						  TKFeat
-						  TKFillet
-						  TKG2d
-						  TKG3d
-						  TKGeomAlgo
-						  TKGeomBase
-						  TKHLR
-						  TKIGES
-						  TKIVtk
-						  TKIVtkDraw
-						  TKLCAF
-						  TKMath
-						  TKMesh
-						  TKMeshVS
-						  TKMessageModel
-						  TKMessageView
-						  TKOffset
-						  TKOpenGl
-						  TKOpenGles
-						  TKOpenGlesTest
-						  TKOpenGlTest
-						  TKPrim
-						  TKQADraw
-						  TKRWMesh
-						  TKService
-						  TKShapeView
-						  TKShHealing
-						  TKStd
-						  TKStdL
-						  TKSTEP
-						  TKSTEP209
-						  TKSTEPAttr
-						  TKSTEPBase
-						  TKSTL
-						  TKTInspector
-						  TKTInspectorAPI
-						  TKTObj
-						  TKTObjDRAW
-						  TKToolsDraw
-						  TKTopAlgo
-						  TKTopTest
-						  TKTreeModel
-						  TKV3d
-						  TKVCAF
-						  TKView
-						  TKViewerTest
-						  TKVInspector
-						  TKVRML
-						  TKXCAF
-						  TKXDE
-						  TKXDECascade
-						  TKXDEDRAW
-						  TKXDEIGES
-						  TKXDESTEP
-						  TKXMesh
-						  TKXml
-						  TKXmlL
-						  TKXmlTObj
-						  TKXmlXCAF
-						  TKXSBase
-						  TKXSDRAW
+set(OPENCASCADE_COMPONETS TKernel
+							TKMath
+							TKG2d
+							TKG3d
+							TKGeomBase
+							TKBRep
+							TKGeomAlgo
+							TKTopAlgo
+							TKPrim
+							TKBO
+							TKShHealing
+							TKBool
+							TKHLR
+							TKMesh
+							TKService
+							TKV3d
+							TKCDF
+							TKLCAF
+							TKCAF
+							TKBinL
+							TKXmlL
+							TKBin
+							TKXml
+							TKVCAF
+							TKXSBase
+							TKSTEPBase
+							TKSTEPAttr
+							TKSTEP209
+							TKSTEP
+							TKIGES
+							TKXCAF
+							TKXDE
+							TKXDEIGES
+							TKXDESTEP
+							TKSTL
+							TKVRML
+							TKRWMesh
+							TKXmlXCAF
+							TKBinXCAF
 					)
 					
-if(CC_BC_WIN)
-	#list(APPEND OPENCASCADE_COMPONETS )
-
-
-elseif(CC_BC_LINUX)
-	#__conan_import(ffmpeglib dll COMPONENT avcodec avdevice avfilter avformat avutil swresample swscale)
-else()
-	#__conan_import(ffmpeglib dll COMPONENT avcodec avdevice avfilter avformat avutil postproc swresample swscale)
-endif()
-
-
-if(OPENCASCADE_STATIC)
+if(USE_OPENCASCADE_STATIC)
 	__conan_import(opencascade lib COMPONENT ${OPENCASCADE_COMPONETS})
 else()
 	__conan_import(opencascade dll COMPONENT ${OPENCASCADE_COMPONETS})
 endif()
-
-#__conan_import(opencascade lib COMPONENT boost_test)
