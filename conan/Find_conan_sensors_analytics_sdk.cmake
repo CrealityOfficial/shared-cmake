c06e2a4cd0ae74b1e71fb8ef0678ee5486b54719
zenggui <anoob@sina.cn>
2023-09-10 21:50:44 +0800

    feature:[eventtracking]move sdk to cxkernel

--- conan/Find_conan_sensors_analytics_sdk.cmake
+++ conan/Find_conan_sensors_analytics_sdk.cmake
@@ -1,6 +1,2 @@
 #sensors_analytics_sdk target
-if(CC_BC_MAC)
-	__conan_import(sensors_analytics_sdk lib)
-else()
-	__conan_import(sensors_analytics_sdk lib)
-endif()
+__conan_import(sensors_analytics_sdk lib)
