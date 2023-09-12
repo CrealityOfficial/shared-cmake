935f2d94e4c03ff40dbeb2bb6a151c4e5b466712
zenggui <anoob@sina.cn>
2023-09-13 00:08:03 +0800

    posix local

--- conan/Find_conan_colladadom_static.cmake
+++ conan/Find_conan_colladadom_static.cmake
@@ -6,6 +6,6 @@ __cc_find(libxml_static)
 
 __conan_import(colladadom_static lib COMPONENT colladadom141 colladadom_static 
 									 ILIB libxml_static minizip_static zip_static colladadom141
-										pcre_local pcrecpp_local
+										pcre_local pcrecpp_local pcreposix_local
 									 )
 
