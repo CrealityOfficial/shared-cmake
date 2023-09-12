75f5d0ffca795926f3775dd01369fe65d0850a27
zenggui <anoob@sina.cn>
2023-09-12 23:21:11 +0800

    feature:[cmake]

--- conan/Find_conan_colladadom_static.cmake
+++ conan/Find_conan_colladadom_static.cmake
@@ -1,6 +1,11 @@
-# colladadom target
-__cc_find(zip)
-__cc_find(minizip)
-__cc_find(libxml)
+# colladadom_static target
+__cc_find(pcre)
+__cc_find(zip_static)
+__cc_find(minizip_static)
+__cc_find(libxml_static)
 
-__conan_import(colladadom_static lib COMPONENT colladadom_static colladadom141)
+__conan_import(colladadom_static lib COMPONENT colladadom141 colladadom_static 
+									 ILIB libxml_static minizip_static zip_static colladadom141
+										pcre_local pcrecpp_local
+									 )
+
