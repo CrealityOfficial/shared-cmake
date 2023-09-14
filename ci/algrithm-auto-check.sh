327cbc90d9a97640341aca2b20819be207b5db37
anoob <anoob@sina.cn>
2023-09-14 11:52:40 +0800

    feature:[]check

--- ci/algrithm-auto-check.sh
+++ ci/algrithm-auto-check.sh
@@ -5,7 +5,7 @@ origin_path=$PWD
 
 #compile
 echo "compile"
-python3 cmake/ci/conan-cmake.py -t linux
+python3 cmake/ci/conan-cmake.py -t linux || exit -2
 cmake -S . -B linux-build/build/ -DONLY_UNIT_TESTS=ON
 cd linux-build/build/
 ninja all
@@ -14,4 +14,4 @@ cd $origin_path
 #test
 pwd
 echo "run test"
-python3 cmake/ci/algrithm-auto-check.py $1
+python3 cmake/ci/algrithm-auto-check.py $1 || exit -2
