a6a652357c34e127d28fac921fdbf0becc083667
anoob <anoob@sina.cn>
2023-09-16 16:13:56 +0800

    feature:[noigl]cgal

--- conan/Find_conan_cgal.cmake
+++ conan/Find_conan_cgal.cmake
@@ -1,5 +1,6 @@
-__cc_find(Boost)
+__cc_find(boost_header)
+
 if(CONAN_CGAL_ROOT_RELEASE)
 	set(CGAL_INCLUDE_DIRS ${CONAN_CGAL_ROOT_RELEASE}/include/ ${CONAN_CGAL_ROOT_RELEASE}/include/CGAL/Root/)
-	message(STATUS "cgal CGAL_INCLUDE_DIRS : ${CGAL_INCLUDE_DIRS}")
+	__add_include_interface(cgal INTERFACE ${CGAL_INCLUDE_DIRS})	
 endif()
