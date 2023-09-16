63e36ef6c0b223d73e219a7cd60ca442c8f908bd
unknown <cx1959@cxsw.com>
2023-09-16 12:50:55 +0800

    find ovdbutil

--- conan/Find_conan_ovdbutil.cmake
+++ conan/Find_conan_ovdbutil.cmake
@@ -1,6 +1,8 @@
 # ovdbutil target
 if(CC_BC_EMCC)
-__conan_import(ovdbutil lib COMPONENT ovdbutil openvdb)
+	__conan_import(ovdbutil lib COMPONENT ovdbutil openvdb)
 else()
-__conan_import(ovdbutil dll COMPONENT ovdbutil openvdb)
+	__conan_find(trimesh2)
+	__conan_find(tbb)
+	__conan_import(ovdbutil dll ILIB trimesh2)
 endif()
