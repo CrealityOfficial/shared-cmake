1bb6166d1151476b1eca140bf454e6b6cb35ef28
zenggui <anoob@sina.cn>
2023-10-15 01:04:07 +0800

    feature:[]conan

--- ci/conan.py
+++ ci/conan.py
@@ -1,30 +1,49 @@
-'''    cmake_args  (-DCXX_VLD=ON/OFF vld debug   
-                    -DRENDER_DOC=ON/OFF renderdoc debug
-                    -MIX_PYTHON=ON/OFF python 
-                )
+'''
 '''
                 
 import sys;
 
 sys.path.append(sys.path[0] + '/../python/')
 sys.path.append(sys.path[0] + '/../pmodules/')
-source_path = sys.path[0] + '/../../'
+cmake_path = sys.path[0] + '/../'
 
-import ci_cmake
-cmake = ci_cmake.CMake(source_path)
+import ci_conan
+conan = ci_conan.Conan(cmake_path)
 
-cmake_args = ""
+channel_name = 'desktop'
+
+recipe_type = 'recipe|xxx/x.x.x'
+'''
+recipe|xxx/x.x.x)
+patch|./patches/cxbin.patch
+subs|./subs/xxx.subs
+whole
+'''
+
+upload = True
 #parse args
 try:
-    opts, args = getopt.getopt(sys.argv[1:], '',['cmake_args='])
-    print("getopt.getopt -> :" + str(opts))
+    opts, args = getopt.getopt(argv,"n:t:u:")
 except getopt.GetoptError:
-    print("_parse_args error.")
+    print("create.py -n <name>")
     sys.exit(2)
 for opt, arg in opts:
-    if opt in ('--cmake_args'):
-        cmake_args = arg 
-       
-print("cmake args : {}".format(cmake_args))       
-cmake.print()
-cmake.build(cmake_args)
+    if opt in ("-n"):
+        channel_name = arg
+    if opt in ("-t"):
+        recipe_type = arg
+    if opt in ("-u"):
+        if arg != "True":
+            upload = False
+            
+if recipe_type.startswith('recipe'):
+    conan.create_one(recipe_type.split('|')[1], channel_name, upload)
+    
+if recipe_type.startswith('patch'):
+    conan.create_from_patch_file(recipe_type.split('|')[1], channel_name, upload)
+    
+if recipe_type.startswith('subs'):
+    conan.create_from_subs_file(recipe_type.split('|')[1], channel_name, upload)
+    
+if recipe_type.startswith('whole'):
+    conan.create_whole(channel_name, upload)
