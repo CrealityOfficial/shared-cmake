41dd95736eaee02c73777148d8bd027caaaee149
anoob <anoob@sina.cn>
2024-06-09 00:02:07 +0800

    1

--- conan/Find_conan_openexr.cmake
+++ conan/Find_conan_openexr.cmake
@@ -1,9 +1,22 @@
+#openexr target
 
-set(EXR_COMPONENTS Iex IexMath IlmThread Imath OpenEXR OpenEXRUtil)
+if(TARGET OpenEXR::OpenEXR)
+	return()
+endif()
 
-if(CC_BC_WIN)
-	__conan_import(openexr dll COMPONENT ${EXR_COMPONENTS})
-	__add_target_interface(Imath DEF IMATH_DLL)
-else()
-	__conan_import(openexr lib COMPONENT ${EXR_COMPONENTS})
+set(imath_cmake "${CONAN_OPENEXR_ROOT_RELEASE}/lib/cmake/Imath/ImathTargets.cmake")
+set(openexr_cmake "${CONAN_OPENEXR_ROOT_RELEASE}/lib/cmake/OpenEXR/OpenEXRTargets.cmake")
+if(EXISTS ${imath_cmake} AND EXISTS ${openexr_cmake})
+	include(${imath_cmake})
+	include(${openexr_cmake})
+	
+	find_package(Threads)
+	__cc_find(zlib_static)
+	
+	__assert_target(Imath::Imath)
+	__assert_target(OpenEXR::Iex)
+	__assert_target(OpenEXR::IlmThread)
+	__assert_target(OpenEXR::OpenEXRCore)
+	__assert_target(OpenEXR::OpenEXR)
+	__assert_target(OpenEXR::OpenEXRUtil)
 endif()
