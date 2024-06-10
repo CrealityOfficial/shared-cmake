577b3bd56420788cab1948dffba2a5ffcb27c85b
anoob <anoob@sina.cn>
2024-06-10 21:07:03 +0800

    1

--- conan/Find_conan_embree.cmake
+++ conan/Find_conan_embree.cmake
@@ -1,7 +1,10 @@
 #embree target
 
-set(embree_cmake "${CONAN_EMBREE_ROOT_RELEASE}/lib/cmake/embree-3.13.5/embree-targets.cmake")
-if(EXISTS ${embree_cmake})
-	include(${embree_cmake})
-	__copy_find_targets(embree)
+if(TARGET embree)
+	return()
 endif()
+
+include(${CONAN_EMBREE_ROOT_RELEASE}/lib/cmake/embree-3.13.5/embree-targets.cmake)
+set(_IMPORT_PREFIX ${CONAN_EMBREE_ROOT_DEBUG})
+include(${CONAN_EMBREE_ROOT_DEBUG}/lib/cmake/embree-3.13.5/embree-targets-debug.cmake)
+__copy_find_targets(embree)
