e1f50a9593c06345cdb00dacde383edbdc4c5b2d
zenggui <anoob@sina.cn>
2024-08-01 19:49:32 +0800

    1

--- ccglobal/debugger.h
+++ ccglobal/debugger.h
@@ -15,6 +15,7 @@ namespace ccglobal
 		virtual void clear_visual(const std::string& pattern) = 0;
 		virtual void visual_polygon(const std::string& name, const Polygon& polygon, const trimesh::vec4& color, float width) = 0;
 		virtual void visual_color_polygon(const std::string& name, const Polygon& polygons, const std::vector<trimesh::vec4>& colors, float width) = 0;
+		virtual void visual_color_points(const std::string& name, const Polygon& points, const trimesh::vec4& color, float point_size) = 0;
 	};
 }
 
